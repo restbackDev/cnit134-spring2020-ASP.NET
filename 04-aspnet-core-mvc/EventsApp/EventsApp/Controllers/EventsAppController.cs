888476aa44985753f289e01eef460f486996fde0
restbackDev <jasonez_03@yahoo.com>
2020-04-17 02:57:05 -0700

    p.27 updated EventsApp Contoller + Course.cs + Welcome.cshtml

--- 04-aspnet-core-mvc/EventsApp/EventsApp/Controllers/EventsAppController.cs
+++ 04-aspnet-core-mvc/EventsApp/EventsApp/Controllers/EventsAppController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using System.Text.Encodings.Web;
+using EventsApp.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -17,12 +18,34 @@ namespace EventsApp.Controllers
             return View();
         }
 
-        public IActionResult Welcome(string name, int seats = 1)
+        public IActionResult Welcome()
         {
-            ViewData["Message"] = "The course " + name + " has ";
-            ViewData["Seats"] = seats;
+            List<Course> CourseList = GetCourses();
+            ViewData["Message"] = CourseList;
 
             return View();
+        }
+
+        private static List<Course> GetCourses()
+        {
+            var c = new Course();
+            List<Course> clist = new List<Course>();
+
+            c.Id = 1;
+            c.Title = "CNIT 131";
+            c.StartDate = Convert.ToDateTime("01/20/2020");
+            c.Type = "Online";
+
+            clist.Add(c);
+
+            var c1 = new Course();
+            c1.Id = 2; c1.Title = "CNIT 132";
+            c1.StartDate = Convert.ToDateTime("01/15/2020");
+            c1.Type = "F2F";
+
+            clist.Add(c1);
+
+            return clist;
         }
     }
 
--- 04-aspnet-core-mvc/EventsApp/EventsApp/Models/Course.cs
+++ 04-aspnet-core-mvc/EventsApp/EventsApp/Models/Course.cs
@@ -1,10 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
 namespace EventsApp.Models
 {
     public class Course
     {
-        public Course()
-        {
-        }
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+        public string Type { get; set; }
     }
 }
