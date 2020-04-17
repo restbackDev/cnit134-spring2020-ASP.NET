04ed96bb0e2fd56dafde105bacf404642c1eda2c
restbackDev <jasonez_03@yahoo.com>
2020-04-17 01:20:35 -0700

    added EventsApp folder via View

---
+++ 04-aspnet-core-mvc/EventsApp/EventsApp/Controllers/EventsAppController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EventsApp.Controllers
+{
+    public class EventsAppController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello, {name}, your ID is {ID}");
+        }
+    }
+
+
+}
